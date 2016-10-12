555b9cda3ae13a0d90c8a4de0325430537023a35
csyjet <gzcsy1213@gmail.com>
2016-10-11 20:47:09 -0700

    test

--- TheBlackArts/src/core/Action.java
+++ TheBlackArts/src/core/Action.java
@@ -1,11 +1,15 @@
 package core;
 
 public class Action extends Card {
-	
+    
+	private String cardType1 = "SlowAction";
+	private String cardType2 = "FastAction";
+    private int hitPoints = 1;
+
 	Action() {
 		
 	}
 	
-	// TODO: Implement action class
+	
 
 }
